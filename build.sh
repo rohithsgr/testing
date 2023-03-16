cd8a4c81c994d14adfe7e7808aba972cf0bee99b
Devopskiller <105400270+rohithsgr@users.noreply.github.com>
2023-03-16 11:30:50 +0530

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+cat <<EOT >Dockerfile
+FROM node:16-alpine as build
+WORKDIR /rohithbuildapp
+COPY package*.json /rohithbuildapp/
+RUN npm install
+COPY . .
+RUN npm run build
+FROM nginx:latest
+COPY --from=build /rohithbuildapp/build/ /usr/share/nginx/html/
+EXPOSE 80
+CMD ["nginx", "-g", "daemon off;"]
+EOT
+
+echo "Completed creating docker file. Below are its contents"
+
+cat Dockerfile
+
+
+echo "Now we will create an image using this docker file with when function using jenkins "
+
+docker build -t rohithsgr/dev:${BUILD_NUMBER} .
